b460d69c2fc9ed9ae9cc1f62669244169a007c9c
Daniel Ribas <ds_ribas@hotmail.com>
2025-04-02 00:13:32 -0300

    feat: update Phone class

--- src/main/java/com/example/domain/value/Phone.java
+++ src/main/java/com/example/domain/value/Phone.java
@@ -4,15 +4,21 @@ import java.util.Objects;
 import java.util.regex.Pattern;
 
 public record Phone(String number) {
-    private static final Pattern PHONE_PATTERN = Pattern.compile("^\\+?[1-9]\\d{1,14}$");
+    private static final Pattern PHONE_PATTERN = Pattern.compile("^[1-9]\\d{1,14}$");
 
     public Phone {
         Objects.requireNonNull(number);
 
-        var matcher = PHONE_PATTERN.matcher(number);
+        var normalized = getNormalized();
+
+        var matcher = PHONE_PATTERN.matcher(normalized);
         if (!matcher.matches()) {
             throw new IllegalArgumentException("Invalid phone number format, must be in E.164 format");
         }
+    }
+
+    String getNormalized() {
+        return Objects.requireNonNull(number).replaceAll("\\D", "");
     }
 
     @Override
